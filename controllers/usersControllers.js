ce6d763301d32a1b09f6b2e1b0d47757e8acf8d2
VPresich <valya.alexandrova.presich@gmail.com>
2024-06-02 21:50:25 +0200

    Added function for saved avatar in google cloud storage

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -5,6 +5,7 @@ import fs from 'node:fs/promises';
 import User from '../models/user.js';
 import HttpError from '../helpers/HttpError.js';
 import ctrlWrapper from '../helpers/ctrlWrapper.js';
+import { uploadFileToGCS } from '../helpers/upload.js';
 
 export const getCurrent = ctrlWrapper(async (req, res, next) => {
   const { email, subscription, avatarURL } = req.user;
@@ -27,29 +28,61 @@ export const updateSubscription = ctrlWrapper(async (req, res, next) => {
     .json({ email: updatedUser.email, subscription: updatedUser.subscription });
 });
 
+// export const updateAvatar = ctrlWrapper(async (req, res, next) => {
+//   const { id, avatarURL: oldAvatarURL } = req.user;
+//   const { path: tempUpload, originalname } = req.file;
+
+//   const avatarsDir = path.resolve('public', 'avatars');
+
+//   // Create unique avatars name for the user
+//   const avatarName = `${id}avatar${path.extname(originalname)}`;
+
+//   // Avatars name with full path
+//   const resultUpload = path.resolve(avatarsDir, avatarName);
+
+//   // Change size of the user file
+//   await resizeImage(tempUpload, 250, 250);
+
+//   // Delete old avatar from dir
+//   await deleteAvatar(oldAvatarURL);
+
+//   // Move tempUpload to the avatars dir
+//   await fs.rename(tempUpload, resultUpload);
+
+//   // Change field in DB
+//   const avatarURL = path.join('avatars', avatarName);
+//   const updatedUser = await User.findByIdAndUpdate(
+//     id,
+//     { avatarURL },
+//     { new: true }
+//   );
+//   if (!updatedUser) {
+//     throw HttpError(401);
+//   }
+//   res.status(200).json({
+//     avatarURL,
+//   });
+// });
+
 export const updateAvatar = ctrlWrapper(async (req, res, next) => {
   const { id, avatarURL: oldAvatarURL } = req.user;
   const { path: tempUpload, originalname } = req.file;
 
-  const avatarsDir = path.resolve('public', 'avatars');
-
   // Create unique avatars name for the user
-  const avatarName = `${id}avatar${path.extname(originalname)}`;
+  const gcsFileName = `${id}avatar${path.extname(originalname)}`;
 
   // Avatars name with full path
-  const resultUpload = path.resolve(avatarsDir, avatarName);
+  const gcsPath = 'avatars/' + gcsFileName;
 
   // Change size of the user file
   await resizeImage(tempUpload, 250, 250);
 
-  // Delete old avatar from dir
-  await deleteAvatar(oldAvatarURL);
-
-  // Move tempUpload to the avatars dir
-  await fs.rename(tempUpload, resultUpload);
+  // upload to ToGCS
+  let avatarURL = await uploadFileToGCS(tempUpload, gcsPath);
+  avatarURL = `https://storage.cloud.google.com/${process.env.GOOGLE_BUCKET_NAME}/${gcsPath}`;
+  await deleteAvatar(tempUpload);
 
   // Change field in DB
-  const avatarURL = path.join('avatars', avatarName);
   const updatedUser = await User.findByIdAndUpdate(
     id,
     { avatarURL },
@@ -63,6 +96,25 @@ export const updateAvatar = ctrlWrapper(async (req, res, next) => {
   });
 });
 
+// export const getAvatar = ctrlWrapper(async (req, res, next) => {
+//   const { id, avatarURL } = req.user;
+
+//   const user = await User.findById(id);
+//   if (!user) {
+//     throw HttpError(401);
+//   }
+//   if (!user.avatarURL) {
+//     throw HttpError(404, 'Avatar not found');
+//   }
+
+//   if (avatarURL.includes('gravatar')) {
+//     return res.redirect(avatarURL);
+//   }
+
+//   const avatarDir = path.resolve('public', avatarURL);
+//   res.status(200).sendFile(avatarDir);
+// });
+
 export const getAvatar = ctrlWrapper(async (req, res, next) => {
   const { id, avatarURL } = req.user;
 
@@ -73,13 +125,7 @@ export const getAvatar = ctrlWrapper(async (req, res, next) => {
   if (!user.avatarURL) {
     throw HttpError(404, 'Avatar not found');
   }
-
-  if (avatarURL.includes('gravatar')) {
-    return res.redirect(avatarURL);
-  }
-
-  const avatarDir = path.resolve('public', avatarURL);
-  res.status(200).sendFile(avatarDir);
+  res.status(200).json({ avatarURL });
 });
 
 // Auxiliary functions
